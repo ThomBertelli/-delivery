221a1af5826cc0bddddefa55be3b0a1091dbb310
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-02-20 19:07:29 -0300

    test(store): add test to validate if the name is filled
    ,

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Store, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "should be valid when name is filled" do
+      store = Store.new name: "Greatest store ever!"
+      # expect(store.valid?).to eq true
+      expect(store).to be_valid
+    end
+  end
 end
