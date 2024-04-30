6c10a0f1d8efd0d3b742d4070757f36ade65f6b0
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-04-30 08:11:15 -0300

    corrige typo

--- spec/requests/api/stores_spec.rb
+++ spec/requests/api/stores_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe "/stores", type: :request do
   let (:user) {
     user = User.new(
-      email: "user@example.com",password: "123456", password_confirmation: "123456"
+      email: "user@example.com",password: "123456", password_confirmation: "123456", role: "seller"
     )
     user.save!
     user
@@ -28,9 +28,10 @@ RSpec.describe "/stores", type: :request do
       store = Store.create! valid_attributes
       get(
         "/stores/#{store.id}",
-        headers:
-          {"Accept" => "application/json"},
-          "Authorization" => "Bearer #{signed_in["token"]}"
+        headers:{
+          "Accept" => "application/json",
+          "Authorization" => "Bearer #{signed_in['token']}"
+        }
       )
       json = JSON.parse(response.body)
       expect(json["name"]).to eq valid_attributes[:name]
