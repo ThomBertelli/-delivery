d9cfe72f5b44c821d3a195028464148350138adc
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-05-19 19:09:15 -0300

    cria model order

---
+++ app/models/order.rb
@@ -0,0 +1,12 @@
+class Order < ApplicationRecord
+  belongs_to :buyer, class_name: "User"
+  belongs_to :store
+
+  validate :buyer_role
+
+  def buyer_role
+    if !buyer.buyer?
+      errors.add(:buyer, "should be a 'user.buyer'")
+    end
+  end
+end
---
+++ db/migrate/20240519215351_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[7.1]
+  def change
+    drop_table :orders, if_exists: true
+
+    create_table :orders do |t|
+      t.references :buyer, foreign_key: { to_table: :users }, null: false
+      t.references :store, foreign_key: true, null: false
+      t.timestamps
+    end
+  end
+end
