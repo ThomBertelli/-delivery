751998bd5fa34d40f2559cc2579679742e404371
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-05-19 19:43:52 -0300

    impede que produtos de diferentes lojas sejam adicionada ao mesmo pedido

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,22 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validate :store_product
+
+  private
+
+  def store_product
+    if product.store != order.store
+      errors.add(
+        :product,
+        "product should belongs to 'Store': #{order.store.name}"
+      )
+
+    end
+  end
+
+
+
+
 end
